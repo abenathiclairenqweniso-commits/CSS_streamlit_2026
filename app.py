0cb1ebe96599d4c118f9ca2c2d24a0de8165531c
abenathiclairenqweniso-commits <abenathiclairenqweniso@gmail.com>
2026-02-02 05:58:19 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 27 17:32:56 2026
+
+@author: BBarsch
+"""
+
+import streamlit as st
+
+st.write("Biochemistry Hons Project")
+
+st.title("Investigating the functional interplay between Cdc37 and Hop in mammalian cancer cells")
+
+st.write("20 October 2025")
+
+st.header("3")
+
+number = st.slider("Pick a number", 1, 100)
+st.write(f"You picked: {number}")
